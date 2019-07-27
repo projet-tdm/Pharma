5ff76b2193857da6a077e92ffac8ba9a16c7fb1e
Amalguittoum <fa_guittoum@esi.dz>
2019-07-27 17:53:56 +0100

    add bottom navigation

--- Pharma/app/src/main/java/com/example/pharma/MainActivity.kt
+++ Pharma/app/src/main/java/com/example/pharma/MainActivity.kt
@@ -2,12 +2,44 @@ package com.example.pharma
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Getting the Navigation Controller
+        navController = Navigation.findNavController(this, R.id.fragment)
+
+        //Setting the navigation controller to Bottom Nav
+        bottomNav.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.identification-> hideBottomNav()
+                R.id.inscription-> hideBottomNav()
+                R.id.forgot-> hideBottomNav()
+                else->showBottomNav()
+
+            }
+        }
+
+
     }
+    private fun showBottomNav() {
+        bottomNav.visibility = View.VISIBLE
+
+    }
+    private fun hideBottomNav() {
+        bottomNav.visibility = View.GONE
+
+    }
+
 }
--- Pharma/app/src/main/java/com/example/pharma/Pharmacies.kt
+++ Pharma/app/src/main/java/com/example/pharma/Pharmacies.kt
@@ -7,22 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_pharmacies.*
 
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 
 class Pharmacies : Fragment() {
 
@@ -32,10 +22,7 @@ class Pharmacies : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pharmacies, container, false)
-        if(activity is AppCompatActivity){
-            (activity as AppCompatActivity).setSupportActionBar(toolbar)
-            (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        }
+
 
     }
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -44,7 +31,7 @@ class Pharmacies : Fragment() {
         val vm= ViewModelProviders.of(activity!!).get(MyModel::class.java)
         val adapter = CustomAdapterPharmacie(context!!,vm.list)
         listpharmacie.adapter = adapter
-        listpharmacie.setOnItemClickListener(AdapterView.OnItemClickListener { parent, view, position, id ->
+        listpharmacie.setOnItemClickListener(AdapterView.OnItemClickListener { _, view, position, id ->
 
             var bundle = Bundle()
             bundle.putInt("id",position)
