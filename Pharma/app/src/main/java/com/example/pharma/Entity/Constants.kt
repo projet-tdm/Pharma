33c1c1ec324a5380840bfd76c10c11b48cd8c310
Amalguittoum <fa_guittoum@esi.dz>
2019-09-13 18:10:49 +0100

    preparation demo

--- Pharma/app/src/main/java/com/example/pharma/Entity/Constants.kt
+++ Pharma/app/src/main/java/com/example/pharma/Entity/Constants.kt
@@ -2,8 +2,8 @@ package com.example.pharma.Entity
 
 
 
-val baseUrl= "http://192.168.43.128/"
-val baseUrl1= "http://192.168.43.128:8083/"
-val twilioURL= "http://192.168.43.128:5000/"
-val usersURL= "http://192.168.43.128:8082/"
-val paymentURL = "http://bd9449d9.ngrok.io"
+val baseUrl= "http://192.168.137.123/"
+val baseUrl1= "http://192.168.137.123:8084/"
+val twilioURL= "http://192.168.137.123:5000/"
+val usersURL= "http://192.168.137.123:8082/"
+val paymentURL = "http://1788f05a.ngrok.io"
--- Pharma/app/src/main/java/com/example/pharma/mapFragment.kt
+++ Pharma/app/src/main/java/com/example/pharma/mapFragment.kt
@@ -167,5 +167,27 @@ class mapFragment : Fragment(),OnMapReadyCallback, GoogleMap.OnInfoWindowClickLi
         mapFragment?.getMapAsync(this)
     }
 
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            101 -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return
+            }
+
+            // Add other 'when' lines to check for other
+            // permissions this app might request.
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
 
 }
